7a39ed5acc5ea5aa92ca1067ac5df7a738cfef4f
Markus Montonen <montonen.markus@gmail.com>
2026-06-16 12:06:24 +0300

    Fix

--- functions/src/email/general.template.ts
+++ functions/src/email/general.template.ts
@@ -1,11 +1,11 @@
-/* eslint-disable max-len */
+
 export function getGeneralTemplate(title: string, body: string) {
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Kiira.app ilmoitus</title>
+<title>Finnut ilmoitus</title>
 <style>
   body {
     font-family: Arial, sans-serif;
